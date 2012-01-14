805bc1a476146afdd9cd296f57c106e15ebbf612
Daniel Hawton <dhawton@zjxartcc.org>
2012-01-14 15:34:55 -0600

    Small fix, remove ZJX specific information from COMMS

--- func/COMMS.php
+++ func/COMMS.php
@@ -31,34 +31,4 @@ $IDS->db_query($db,$res,"SELECT `FullName` FROM `Facilities` WHERE `FacilityID`=
 $row = mysql_fetch_assoc($res);
 ?>
 <center><font color="#ffcc00"><?=$row['FullName']?> Communications Table</font></center>
-<?php
-$f = $_SESSION['facility'];
-if ($f != "ZJX") {
-        $IDS->db_query($db,$res,"SELECT `IATA`,`FullName` FROM `Fields` WHERE `FacilityID`='".$_SESSION['facility']."' AND `Uncontrolled` NOT LIKE '1' ORDER BY `IATA`");
-} else {
-        $IDS->db_query($db,$res,"SELECT `IATA`,`FullName` FROM `Fields` WHERE `Uncontrolled` NOT LIKE '1' ORDER BY `IATA`");
-}
-?>
-<center><table border="0" cellpadding="5" cellspacing="0" style="width: 50%; color: #fff;">
-<tr><td colspan="3" style="text-align: center; color: yellow; text-decoration: underline;">En-Route Control</td></tr>
-<?php
-$roster->db_build($rdb);
-$roster->db_query($rdb,$rres, "SELECT `position`,`freq`,`name` FROM `positions` WHERE `facility`='En-' ORDER BY `type`,`pos`");
-while ($rrow = mysql_fetch_assoc($rres)) { echo "<tr><td>".$rrow['position']."</td><td>".$rrow['name']."</td><td>".$rrow['freq']."</td></tr>"; }
-?>
-<tr><td colspan="3">&nbsp;</td></tr>
-<?php
-while ($row = mysql_fetch_assoc($res))
-{
-	$roster->db_query($rdb,$rres, "SELECT `position`,`freq`,`name` FROM `positions` WHERE `position` LIKE \"".$row['IATA']."%\" AND `position` NOT LIKE \"%CTR\" ORDER BY `type` DESC,`pos`");
-	echo '<tr><td colspan="3" style="text-align: center; color: white; text-decoration: underline;">'.$row['IATA'] . ' - '. $row['FullName']. '</td></tr>';
-	if (mysql_num_rows($rres) == 0) { echo '<tr><td colspan="3" style="text-align: center;"><i>No positions found...</i></td></tr>'; }
-	while ($rrow = mysql_fetch_assoc($rres))
-	{
-		echo '<tr><td>'.$rrow['position'].'</td><td>'.$rrow['name'].'</td><td>'.$rrow['freq'].'</td></tr>';
-	}
-	echo '<tr><td colspan="3">&nbsp;</td></tr>';
-}
-?>
-</table></center>
-
+<br><br>This file needs to be modified.
